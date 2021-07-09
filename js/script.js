2e9160084204a5ecca4e7c216c2ccda80aa8b30d
takahiro okada <t.okada@koshikiad.com>
2021-07-09 18:02:06 +0900

    バリデーション追加

--- js/script.js
+++ js/script.js
@@ -6,35 +6,43 @@ addButton.addEventListener("click", (e) => {
   const input = document.getElementById("add-todo");
   const text = input.value;
 
-  // タスク行
-  const li = document.createElement("li");
-  li.className = "todo__item";
-  li.innerText = text;
+  if( text !== ""){
+    // タスク行
+    const li = document.createElement("li");
+    li.className = "todo__item";
+    li.innerText = text;
 
-  // 完了ボタン
-  const competeButton = document.createElement("button");
-  competeButton.className = "todo__incomplete"
-  competeButton.innerText = "完了"
+    // 完了ボタン
+    const competeButton = document.createElement("button");
+    competeButton.className = "todo__incomplete"
+    competeButton.innerText = "完了"
 
-  // 削除ボタン
-  const deleteButton = document.createElement("button");
-  deleteButton.className = "todo__delete"
-  deleteButton.innerText = "削除"
+    // 削除ボタン
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "todo__delete"
+    deleteButton.innerText = "削除"
 
-  // タスクを追加する
-  const ul = document.getElementById("incomplete-list")
-  let newTodo = ul.appendChild(li);
-  let comp = li.appendChild(competeButton);
-  let compButton = li.appendChild(comp)
+    // タスクを追加する
+    const ul = document.getElementById("incomplete-list")
+    let newTodo = ul.appendChild(li);
+    let comp = li.appendChild(competeButton);
+    let compButton = li.appendChild(comp)
+    localStorage.setItem("mylist", JSON.stringify(compButton));
 
-  compButton.addEventListener("click",(e)=> {
-    const ul = document.getElementById("complete-list");
-    ul.appendChild(newTodo);
-    competeButton.remove();
-    newTodo.appendChild(deleteButton);
-  })
+    let saved = localStorage.getItem("mylist");
 
-  deleteButton.addEventListener("click",(e)=>{
-    newTodo.remove();
-  })
-});
+    compButton.addEventListener("click",(e)=> {
+      const ul = document.getElementById("complete-list");
+      ul.appendChild(newTodo);
+      competeButton.remove();
+      newTodo.appendChild(deleteButton);
+    })
+
+    deleteButton.addEventListener("click",(e)=>{
+      localStorage.clear();
+      newTodo.remove();
+    })
+  } else {
+    alert("ToDoを追加してください")
+  }
+  });
